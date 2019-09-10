c95a531af85dbdf76ad23de3124e483c2c76f96d
MICHAEL WILSON <wilsonmi@otc.edu>
2019-09-10 14:07:13 -0500

    Messed up somwehre in Javascript

--- startup_project/doStuff.js
+++ startup_project/doStuff.js
@@ -1,15 +1,42 @@
 //Implement some javascript to load data from XML into HTMl
 
-var xhttp = new XMLHttpRequest();
+//1st function 
 
-xhttp.onreadystatechange = function() 
-{
-    if (this.readyState == 4 && this.status == 200)
+    //This loads the XML document with an XMLHttpRequest
+
+    function loadXMLDoc(filename)
     {
-        document.getElementById("demo").innerHTML = xhttp.responseText;
-    }//end data transfer
-};
-//end function
-
-xhttp.open("GET", "characters.xml", true);
-xhttp.send();
+    if (window.ActiveXObject)
+      {
+      xhttp = new ActiveXObject("Msxml2.XMLHTTP");
+      }
+    else
+      {
+      xhttp = new XMLHttpRequest();
+      }
+    xhttp.open("GET", filename, false);
+    try {xhttp.responseType = "msxml-document"} catch(err) {} // Helping IE11
+    xhttp.send("");
+    return xhttp.responseXML;
+    }
+    
+    function displayResult()
+    {
+    xml = loadXMLDoc("characters.xml");
+    xsl = loadXMLDoc("style.xsl");
+    // code for IE
+    if (window.ActiveXObject || xhttp.responseType == "msxml-document")
+      {
+      ex = xml.transformNode(xsl);
+      document.getElementById("demo").innerHTML = ex;
+      }
+    // code for Chrome, Firefox, Opera, etc.
+    else if (document.implementation && document.implementation.createDocument)
+      {
+      xsltProcessor = new XSLTProcessor();
+      xsltProcessor.importStylesheet(xsl);
+      resultDocument = xsltProcessor.transformToFragment(xml, document);
+      document.getElementById("demo").appendChild(resultDocument);
+      }
+    }
+    
---
+++ startup_project/style.xsl
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--The point of xsl is to style xml elements and attributes-->
+<!--Need to incorporate xml version> -->
+
+<xsl:stylesheet version="1.0"
+ xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+<xsl:template match="/">  <!-- Applies to the top element of an xml doc-->
+
+<!--Create a place for the xml to go into -->
+
+<h1>Final Fantasy Characters</h1>
+<table border='3'>
+    <tr bgcolor="hsla(89, 43%, 51%, 0.3)">
+        <th style="text-align:left">Character Name</th>
+        <th style="text-allgn:left">Personallity</th>
+        <th style="text-allgn:left">Weapon</th>
+        <th style="text-allgn:left">Gender</th>
+    </tr>
+    <!--For each Chracter record in Characters -->
+    <xsl:for-each select="characters/character">
+    <tr>
+    <!--Grab the value of each character property -->
+        <td><xsl:value-of select ="name" /></td>
+        <td><xsl:value-of select ="personallity" /></td>
+        <td><xsl:value-of select = "weapon"/></td>
+        <td><xsl:value-of select = "gender"/></td>
+    </tr>
+    </xsl:for-each>
+</table>
+</xsl:template>
+
+</xsl:stylesheet>
+
+
+
